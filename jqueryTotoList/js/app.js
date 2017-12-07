2f74eaf134d356f50ad8f0c5ee4073ba19c9b27a
wangqiang <wangqiangweb@163.com>
2017-12-07 16:38:11 +0800

    面向对象搭建

---
+++ jqueryTotoList/js/app.js
@@ -0,0 +1,14 @@
+var myTodoList = (function(){
+	//常量
+	//默认配置
+	
+	
+	function myTodoList(){}
+	//添加任务
+	myTodoList.prototype.addTask=function(){}
+	//删除任务
+	myTodoList.prototype.removeTask=function(){}
+	//保存任务
+	myTodoList.prototype.saveTask=function(){}
+	return myTodoList;
+})()
